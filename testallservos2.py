a78336b36d7c44be6df042bf7e83052801bf8a70
hokocodes <hokocodes@gmail.com>
2025-02-08 17:40:39 -0600

    Update testallservos2.py

--- testallservos2.py
+++ testallservos2.py
@@ -1,6 +1,6 @@
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
-pwm.set_pwm_freq(1000)
+kit.set_pwm_freq(1000)
 kit.servo[0].set_pulse_width_range(1000, 2000)
 
 kit.servo[0].angle = 180
