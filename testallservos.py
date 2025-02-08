7bce7ac567518e1526c06572cd22f7083aec0536
hokocodes <hokocodes@gmail.com>
2025-02-08 16:30:46 -0600

    changed to if statements

--- testallservos.py
+++ testallservos.py
@@ -24,46 +24,46 @@ while True:
 
     servo_number = r  # Change this to test other servos (0-5)
     
-    match servo_number:
-        case 0:
-            kit.continuous_servo[0].throttle = 1
-            print("Servo 0, Throttle = 1")
-            time.sleep(1)
-            kit.continuous_servo[0].throttle = -1
-            print("Servo 0, Throttle = -1")
-            time.sleep(1)
-        case 1:
-            kit.continuous_servo[1].throttle = 1
-            print("Servo 1, Throttle = 1")
-            time.sleep(1)
-            kit.continuous_servo[1].throttle = -1
-            print("Servo 1, Throttle = -1")
-            time.sleep(1)
-        case 2:
-            kit.continuous_servo[2].throttle = 1
-            print("Servo 2, Throttle = 1")
-            time.sleep(1)
-            kit.continuous_servo[2].throttle = -1
-            print("Servo 2, Throttle = -1")
-            time.sleep(1)
-        case 3:
-            kit.continuous_servo[3].throttle = 1
-            print("Servo 3, Throttle = 1")
-            time.sleep(1)
-            kit.continuous_servo[3].throttle = -1
-            print("Servo 3, Throttle = -1")
-            time.sleep(1)
-        case 4:
-            kit.continuous_servo[4].throttle = 1
-            print("Servo 4, Throttle = 1")
-            time.sleep(1)
-            kit.continuous_servo[4].throttle = -1
-            print("Servo 4, Throttle = -1")
-            time.sleep(1)
-        case 5:
-            kit.continuous_servo[5].throttle = 1
-            print("Servo 5, Throttle = 1")
-            time.sleep(1)
-            kit.continuous_servo[5].throttle = -1
-            print("Servo 5, Throttle = -1")
-            time.sleep(1)
+    if servo_number == "0":
+        kit.continuous_servo[0].throttle = 1
+        print("Servo 0, Throttle = 1")
+        time.sleep(1)
+        kit.continuous_servo[0].throttle = -1
+        print("Servo 0, Throttle = -1")
+        time.sleep(1)
+    elif servo_number == "1":
+        kit.continuous_servo[1].throttle = 1
+        print("Servo 1, Throttle = 1")
+        time.sleep(1)
+        kit.continuous_servo[1].throttle = -1
+        print("Servo 1, Throttle = -1")
+        time.sleep(1)
+    elif servo_number == "2":
+        kit.continuous_servo[2].throttle = 1
+        print("Servo 2, Throttle = 1")
+        time.sleep(1)
+        kit.continuous_servo[2].throttle = -1
+        print("Servo 2, Throttle = -1")
+        time.sleep(1)
+    elif servo_number == "3":
+        kit.continuous_servo[3].throttle = 1
+        print("Servo 3, Throttle = 1")
+        time.sleep(1)
+        kit.continuous_servo[3].throttle = -1
+        print("Servo 3, Throttle = -1")
+        time.sleep(1)
+    elif servo_number == "4":
+        kit.continuous_servo[4].throttle = 1
+        print("Servo 4, Throttle = 1")
+        time.sleep(1)
+        kit.continuous_servo[4].throttle = -1
+        print("Servo 4, Throttle = -1")
+        time.sleep(1)
+    elif servo_number == "5":
+        kit.continuous_servo[5].throttle = 1
+        print("Servo 5, Throttle = 1")
+        time.sleep(1)
+        kit.continuous_servo[5].throttle = -1
+        print("Servo 5, Throttle = -1")
+        time.sleep(1)
+
