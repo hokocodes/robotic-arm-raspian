f8a1d9c510e87bb7dd53ca45c9c337afca2962c4
hokocodes <hokocodes@gmail.com>
2025-02-08 17:46:44 -0600

    change delay to 5 seconds

--- testallservos2.py
+++ testallservos2.py
@@ -1,10 +1,13 @@
 from adafruit_servokit import ServoKit
 import time
 kit = ServoKit(channels=16)
+
 kit.servo[0].set_pulse_width_range(1000, 2000)
 
 kit.servo[0].angle = 180
-time.sleep(1)
+print("Servo 0, Angle = 180")
+time.sleep(5)
 kit.servo[0].angle = 0
+print("Servo 0, Angle = 0")
 time.sleep(1)
 
