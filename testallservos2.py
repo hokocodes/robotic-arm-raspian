8cbaf8a44499e5e4f77a83aedeb82de59ccda592
hokocodes <hokocodes@gmail.com>
2025-02-08 17:33:30 -0600

    updated

--- testallservos2.py
+++ testallservos2.py
@@ -1,11 +1,23 @@
-from gpiozero import Servo
-from time import sleep
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
 
-servo = Servo(0)
+import time
+import board
+import pwmio
+from adafruit_motor import servo
 
-servo.value = 0.5
-sleep(1)
-servo.value = 0
-sleep(1)
-servo.value = 1
-sleep(1)
+# This line creates a PWMOut object on Pin D5 with a 50% duty cycle and a frequency of 50Hz.
+pwm = pwmio.PWMOut(board.D0, duty_cycle=2 ** 15, frequency=50)
+
+# Create a servo object.
+servo = servo.Servo(pwm)
+
+while True:
+    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+        servo.angle = angle
+        print("Sleeping for 0.05 seconds")
+        time.sleep(0.15)
+    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+        servo.angle = angle
+        time.sleep(0.15)
