5583dc4336020edac851a335306480c1632b6c6f
hokocodes <hokocodes@gmail.com>
2025-02-08 17:11:02 -0600

    changed to gpiozero

--- testallservos2.py
+++ testallservos2.py
@@ -1,18 +1,12 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
+from gpiozero import Servo
+from time import sleep
 
-"""Simple test for a standard servo on channel 0 and a continuous rotation servo on channel 1."""
-import time
-from adafruit_servokit import ServoKit
-
-# Set channels to the number of servo channels on your kit.
-# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
-kit = ServoKit(channels=16)
+servo = Servo(0)
 
 while True:
-    kit.servo[0].angle = 180
-    print("Servo 0, Angle = 180")
-    time.sleep(10)
-    kit.servo[0].angle = 0
-    print("Servo 0, Angle = 0")
-    time.sleep(10)
+    servo.min()
+    sleep(1)
+    servo.mid()
+    sleep(1)
+    servo.max()
+    sleep(1)
