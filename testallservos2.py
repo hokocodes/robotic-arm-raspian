2d9518fd2ceb2db1e4745915f47faf00da883757
hokocodes <hokocodes@gmail.com>
2025-02-08 19:31:56 -0600

    Update testallservos2.py

--- testallservos2.py
+++ testallservos2.py
@@ -4,13 +4,18 @@ kit = ServoKit(channels=16)
 
 kit.servo[0].set_pulse_width_range(1000, 2000)
 kit.servo[1].set_pulse_width_range(1000, 2000)
+
+kit.servo[2].set_pulse_width_range(1000, 2000)
+kit.servo[3].set_pulse_width_range(1000, 2000)
+kit.servo[4].set_pulse_width_range(1000, 2000)
+kit.servo[5].set_pulse_width_range(1000, 2000)
 kit.servo[1].angle = 180
 print("Servo 1, Angle = 180")
 time.sleep(5)
 
 
 r = input()
-if r == "1":
+if r == "0":
     kit.servo[0].angle = 180
     print("Servo 0, Angle = 180")
     time.sleep(5)
@@ -45,4 +50,80 @@ if r == "1":
     kit.servo[0].angle = 0
     print("Servo 0, Angle = 0")
     time.sleep(5)
-
+elif r == "1":
+    kit.servo[1].angle = 180
+    print("Servo 1, Angle = 180")
+    time.sleep(5)
+    kit.servo[1].angle = 0
+    print("Servo 1, Angle = 0")
+    time.sleep(5)
+elif r == "2":
+    kit.servo[2].angle = 180
+    print("Servo 2, Angle = 180")
+    time.sleep(5)
+    kit.servo[2].angle = 0
+    print("Servo 2, Angle = 0")
+    time.sleep(5)
+elif r == "3":
+    kit.servo[3].angle = 180
+    print("Servo 3, Angle = 180")
+    time.sleep(5)
+    kit.servo[3].angle = 0
+    print("Servo 3, Angle = 0")
+    time.sleep(5)
+elif r == "4":
+    kit.servo[4].angle = 180
+    print("Servo 4, Angle = 180")
+    time.sleep(5)
+    kit.servo[4].angle = 0
+    print("Servo 4, Angle = 0")
+    time.sleep(5)
+elif r == "5":
+    kit.servo[5].angle = 180
+    print("Servo 5, Angle = 180")
+    time.sleep(5)
+    kit.servo[5].angle = 0
+    print("Servo 5, Angle = 0")
+    time.sleep(5)
+elif r == "6":
+    kit.servo[6].angle = 180
+    print("Servo 6, Angle = 180")
+    time.sleep(5)
+    kit.servo[6].angle = 0
+    print("Servo 6, Angle = 0")
+    time.sleep(5)
+elif r == "7":
+    kit.servo[7].angle = 180
+    print("Servo 7, Angle = 180")
+    time.sleep(5)
+    kit.servo[7].angle = 0
+    print("Servo 7, Angle = 0")
+    time.sleep(5)
+elif r == "8":
+    kit.servo[8].angle = 180
+    print("Servo 8, Angle = 180")
+    time.sleep(5)
+    kit.servo[8].angle = 0
+    print("Servo 8, Angle = 0")
+    time.sleep(5)
+elif r == "9":
+    kit.servo[9].angle = 180
+    print("Servo 9, Angle = 180")
+    time.sleep(5)
+    kit.servo[9].angle = 0
+    print("Servo 9, Angle = 0")
+    time.sleep(5)
+elif r == "10":
+    kit.servo[10].angle = 180
+    print("Servo 10, Angle = 180")
+    time.sleep(5)
+    kit.servo[10].angle = 0
+    print("Servo 10, Angle = 0")
+    time.sleep(5)
+elif r == "11":
+    kit.servo[11].angle = 180
+    print("Servo 11, Angle = 180")
+    time.sleep(5)
+    kit.servo[11].angle = 0
+    print("Servo 11, Angle = 0")
+    time.sleep(5)
