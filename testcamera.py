01e4f61a108e6acfeb71f80305cd62ed3d4151d9
hokocodes <hokocodes@gmail.com>
2025-02-15 17:01:28 -0600

    add cam release

--- testcamera.py
+++ testcamera.py
@@ -32,49 +32,13 @@ print("twelve:", twelve.isOpened())
 print("thirteen:", thirteen.isOpened())
 print("fourteen:", fourteen.isOpened())
 print("fifteen:", fifteen.isOpened())
+cameras = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen]
 
-if one.isOpened():
-    ret1, frame1 = one.read()
-    print("ret1:", ret1)
-if two.isOpened():
-    ret2, frame2 = two.read()
-    print("ret2:", ret2)
-if three.isOpened():
-    ret3, frame3 = three.read()
-    print("ret3:", ret3)
-if four.isOpened():
-    ret4, frame4 = four.read()
-    print("ret4:", ret4)
-if five.isOpened():
-    ret5, frame5 = five.read()
-    print("ret5:", ret5)
-if six.isOpened():
-    ret6, frame6 = six.read()
-    print("ret6:", ret6)
-if seven.isOpened():
-    ret7, frame7 = seven.read()
-    print("ret7:", ret7)
-if eight.isOpened():
-    ret8, frame8 = eight.read()
-    print("ret8:", ret8)
-if nine.isOpened():
-    ret9, frame9 = nine.read()
-    print("ret9:", ret9)
-if ten.isOpened():
-    ret10, frame10 = ten.read()
-    print("ret10:", ret10)
-if eleven.isOpened():
-    ret11, frame11 = eleven.read()
-    print("ret11:", ret11)
-if twelve.isOpened():
-    ret12, frame12 = twelve.read()
-    print("ret12:", ret12)
-if thirteen.isOpened():
-    ret13, frame13 = thirteen.read()
-    print("ret13:", ret13)
-if fourteen.isOpened():
-    ret14, frame14 = fourteen.read()
-    print("ret14:", ret14)
-if fifteen.isOpened():
-    ret15, frame15 = fifteen.read()
-    print("ret15:", ret15)
+for i, cam in enumerate(cameras, start=1):
+    if cam.isOpened():
+        ret, frame = cam.read()
+        print(f"ret{i}:", ret)
+    else:
+        print(f"Failed to open camera {i}")
+        cam.release()
+
