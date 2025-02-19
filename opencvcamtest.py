c96fab78d4faaac9124d5bd2810f7dda801bed30
hokocodes <hokocodes@gmail.com>
2025-02-19 12:49:51 -0600

    add v4l2

--- opencvcamtest.py
+++ opencvcamtest.py
@@ -1,7 +1,10 @@
 import cv2
 
 # Open the camera (0 for the default camera)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # Check if the camera is opened successfully
 if not cap.isOpened():
